c947b69d8144b4c657a38fae806d071dd6e47ddf
Aron <aron.liiv@hotmail.com>
2022-11-18 17:53:17 +0200

    changestosignuo

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,19 +1,24 @@
 <template>
-    <form>
-        <span>Email: </span>
-        <input v-model= "form.email" type="text"><br>
-        <span>Password: </span>
-        <input type="password"><br>
+    <form id="form">
+        <span>Email</span>
+        <input v-model="form.email" type="text" placeholder="Email"><br>
+        <span>Password</span>
+        <input v-model="form.password" type="password" placeholder="Password"><br>
+        <p v-if="error">
+            Fix
+        </p>
     </form>
 </template>
 
 <script>
 export default {
-    data: {
-        form: {
+    data: function() {
+        return {
+            form: {
             name: "",
+            password: ""
+        }
         }
     }
-    
 }
 </script>
