ec398cf7e3411b1e91e775754b0b49a8a3bd0a12
Aron <aron.liiv@hotmail.com>
2022-11-18 16:45:27 +0200

    something

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,3 +1,19 @@
 <template>
-    <h3>signuyp page</h3>
+    <form>
+        <span>Email: </span>
+        <input v-model= "form.email" type="text"><br>
+        <span>Password: </span>
+        <input type="password"><br>
+    </form>
 </template>
+
+<script>
+export default {
+    data: {
+        form: {
+            name: "",
+        }
+    }
+    
+}
+</script>
