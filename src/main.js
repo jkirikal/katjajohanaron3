246bae615c89283399528e14e6683fa5a7200889
Aron <aron.liiv@hotmail.com>
2022-11-21 01:41:16 +0200

    kds

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Vuelidate from 'vuelidate'
 
-createApp(App).use(Vuelidate).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,44 +1,61 @@
 <template>
-    <h1>tere</h1>
-    <div>
-       <form id="form">
-        <span>Email</span>
-        <input v-model="form.email" type="text" placeholder="Email"><br>
-        <span>Password</span>
-        <input type="password" v-model.trim="$v.password.$model" 
-        :class="{'is-invalid': $v.password.$error, 'is-valid': !$v.password.$invalid}">
-        
+    <div id="main">
+        <form @submit="kontroll" action="#">
+            <div class="text">
+                <span>Username</span>
+                <input type="text" placeholder="Username">
+            </div><br>
+            <div class="text">
+                <span>Password</span>
+                <input type="password" placeholder="Password" v-model="pass">
+            </div><br>
+            <button type="submit">Submit</button><br>
+            <p v-if="!this.correct" id="warning">
+                Password must contain: One uppercase letter, two lowercase, 
+                one number, one "-" and must be between 8-15 charecters.
+            </p>
         </form>
-     <div>Password is valid!</div>
     </div>
 </template>
 
 <script>
-import { required, minLength, maxLength } from 'vuelidate/lib/validators'
-
 export default {
-    data: function () {
+    data: function() {
         return {
-            password: ""
+            pass: null,
+            correct: true
         }
     },
-    validations: {
-        password: {
-            required,
-            minLength: minLength(8),
-            maxLength: maxLength(15),
+    methods: {
+        kontroll: function(juhtum) {
+            let errorset = /^[A-Z]+(?=.*[0-9])(?=.*[a-z])(?=.*[a-z])(?=.*[A-Z])(?=.*[-]).{8,15}$/.test(this.pass)
+            console.log(errorset)
+            this.correct = errorset
+            juhtum.preventDefault()
+
         }
     }
-
 }
 </script>
 
 <style scoped>
-div {
+#main {
     display: flex;
     justify-content: center;
     align-items: center;
     background-color: aqua;
-    margin: 50px;
+    margin: 100px;
+    border-radius: 25px;
+    padding: 20px;
+}
+.text {
+    display: flex;
+    margin: 20px;
+    margin-left: 50px;
+    margin-right: 50px;
+}
+#warning {
+    color: red;
+    word-wrap: break-word;
 }
 </style>
