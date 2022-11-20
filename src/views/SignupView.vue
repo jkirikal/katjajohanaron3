cde98ab1ab921d3b0698c9d3b5480baee682b4be
Aron <aron.liiv@hotmail.com>
2022-11-20 23:55:21 +0200

    something

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,24 +1,44 @@
 <template>
-    <form id="form">
+    <h1>tere</h1>
+    <div>
+       <form id="form">
         <span>Email</span>
         <input v-model="form.email" type="text" placeholder="Email"><br>
         <span>Password</span>
-        <input v-model="form.password" type="password" placeholder="Password"><br>
-        <p v-if="error">
-            Fix
-        </p>
-    </form>
+        <input type="password" v-model.trim="$v.password.$model" 
+        :class="{'is-invalid': $v.password.$error, 'is-valid': !$v.password.$invalid}">
+        
+        </form>
+     <div>Password is valid!</div>
+    </div>
 </template>
 
 <script>
+import { required, minLength, maxLength } from 'vuelidate/lib/validators'
+
 export default {
-    data: function() {
+    data: function () {
         return {
-            form: {
-            name: "",
             password: ""
         }
+    },
+    validations: {
+        password: {
+            required,
+            minLength: minLength(8),
+            maxLength: maxLength(15),
         }
     }
+
 }
 </script>
+
+<style scoped>
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: aqua;
+    margin: 50px;
+}
+</style>
